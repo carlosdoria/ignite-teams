66a6606f58c9f844cfb27ac410237ef39f992ce0
Carlos Dória <carlosdoria953@gmail.com>
2023-04-22 13:00:37 -0300

    feat: implement FlatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
+  const [groups, setGroups] = useState([]);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
-      <GroupCard title="Galera do ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={<ListEmpty message="Cadraste a primeira turma" />}
+      />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
